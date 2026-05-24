44fb8382b76d90bdd2d55c493496004bcdc8f927
Narendra <narendrakchoudhary5@gmail.com>
2026-05-24 22:17:36 +0530

    Switched from sqlx + pgx to pure pgx for auth service, added route based navigation to frontend.
    Switched to using pgx for the auth service providing better performance
    and better control over query parameters. Changed frontend to use route
    based navigation using react-router.

--- services/auth-service/internal/handler/auth_handler.go
+++ services/auth-service/internal/handler/auth_handler.go
@@ -15,6 +15,7 @@ type AuthHandler struct {
 }
 
 func (h *AuthHandler) AdminRegister(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 	var creds models.OraganisationRegistration
 
 	if err := json.NewDecoder(r.Body).Decode(&creds); err != nil {
@@ -24,13 +25,13 @@ func (h *AuthHandler) AdminRegister(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Check if the organisation already exists.
-	if h.Store.CheckOrgExists(creds.OrganisationName) {
+	if h.Store.CheckOrgExists(ctx, creds.OrganisationName) {
 		http.Error(w, "Organisation already exists", http.StatusBadRequest)
 		return
 	}
 
 	// Check if the admin email already exists.
-	_, err := h.Store.GetUserByEmail(creds.AdminEmail)
+	_, err := h.Store.GetUserByEmail(ctx, creds.AdminEmail)
 	if err == nil {
 		http.Error(w, "Email already registered", http.StatusBadRequest)
 		return
@@ -41,7 +42,7 @@ func (h *AuthHandler) AdminRegister(w http.ResponseWriter, r *http.Request) {
 	creds.AdminPassword = hashedPassword
 
 	// insert new user into database.
-	adminPublic, err := h.Store.RegisterOrgAndAdmin(&creds)
+	adminPublic, err := h.Store.RegisterOrgAndAdmin(ctx, &creds)
 	if err != nil {
 		log.Println(err)
 		http.Error(w, "Could not register organisation", http.StatusInternalServerError)
@@ -67,6 +68,7 @@ func (h *AuthHandler) AdminRegister(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *AuthHandler) AdminLogin(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 	var creds models.AdminLogin
 
 	if err := json.NewDecoder(r.Body).Decode(&creds); err != nil {
@@ -75,22 +77,10 @@ func (h *AuthHandler) AdminLogin(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// get user with the email from the database.
-	user, err := h.Store.GetUserByEmail(creds.AdminEmail)
+	user, err := h.Store.GetUserByEmail(ctx, creds.AdminEmail)
 	// return error if user doesn't already exists or password doesn't match the password.
-	if err != nil || !auth.CheckPassword(creds.AdminPassword, user.Password) {
+	if err != nil || !user.IsOrgAdmin || !auth.CheckPassword(creds.AdminPassword, user.Password) {
 		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
-		return
-	}
-
-	// check if user belongs to the organisation.
-	org, err := h.Store.GetOrgByPublicID(user.OrgID)
-	if err != nil {
-		http.Error(w, "Failed to get organisation", http.StatusInternalServerError)
-		return
-	}
-
-	if org.Name != creds.OrganisationName {
-		http.Error(w, "User doesn't belong to the organisation", http.StatusUnauthorized)
 		return
 	}
 
@@ -129,7 +119,8 @@ func (h *AuthHandler) GetServiceToken(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *AuthHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
-	claims, ok := r.Context().Value("claims").(*models.Claims)
+	ctx := r.Context()
+	claims, ok := ctx.Value("claims").(*models.Claims)
 	if !ok {
 		http.Error(w, "Failed to get claims from the token", http.StatusInternalServerError)
 		return
@@ -138,7 +129,7 @@ func (h *AuthHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	var creds models.User
 
 	// Check if the user is an admin.
-	if !h.Store.CheckUserIsOrgAdmin(userID) {
+	if !h.Store.CheckUserIsOrgAdmin(ctx, userID) {
 		http.Error(w, "User doesn't have admin privileges", http.StatusUnauthorized)
 		return
 	}
@@ -148,6 +139,7 @@ func (h *AuthHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid request", http.StatusBadRequest)
 		return
 	}
+	creds.OrgID = claims.OrgID
 
 	// Hash the user password.
 	hashedPassword, err := auth.HashPassword(creds.Password)
@@ -158,7 +150,7 @@ func (h *AuthHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	creds.Password = hashedPassword
 
 	// insert new user into database.
-	createdUserID, err := h.Store.CreateUser(&creds)
+	createdUserID, err := h.Store.CreateUser(ctx, &creds)
 	if err != nil {
 		http.Error(w, "Could not create user", http.StatusInternalServerError)
 		return
@@ -169,6 +161,7 @@ func (h *AuthHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 	var creds models.User
 
 	if err := json.NewDecoder(r.Body).Decode(&creds); err != nil {
@@ -177,7 +170,7 @@ func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// get user with the email from the database.
-	user, err := h.Store.GetUserByEmail(creds.Email)
+	user, err := h.Store.GetUserByEmail(ctx, creds.Email)
 	// return error if user doesn't already exists or password doesn't match the password.
 	if err != nil || !auth.CheckPassword(creds.Password, user.Password) {
 		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
@@ -192,4 +185,149 @@ func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(map[string]string{"user_name": user.Name, "user_id": user.ID, "access_token": accToken})
+}
+
+func (h *AuthHandler) GetOrgUsers(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	claims, ok := ctx.Value("claims").(*models.Claims)
+	if !ok {
+		http.Error(w, "Failed to get claims from the token", http.StatusInternalServerError)
+		return
+	}
+	userID := claims.Subject
+
+	// Check if the user is an admin.
+	if !h.Store.CheckUserIsOrgAdmin(ctx, userID) {
+		http.Error(w, "User doesn't have admin privileges", http.StatusUnauthorized)
+		return
+	}
+
+	orgID := claims.OrgID
+	users, err := h.Store.GetOrgUsers(ctx, orgID)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Failed to get organisation users", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(users)
+}
+
+func (h *AuthHandler) CreateDepartment(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	claims, ok := ctx.Value("claims").(*models.Claims)
+	if !ok {
+		http.Error(w, "Failed to get claims from the token", http.StatusInternalServerError)
+		return
+	}
+	userID := claims.Subject
+
+	// Check if the user is an admin.
+	if !h.Store.CheckUserIsOrgAdmin(ctx, userID) {
+		http.Error(w, "User doesn't have admin privileges", http.StatusUnauthorized)
+		return
+	}
+
+	var department models.DepartmentRegistration
+
+	if err := json.NewDecoder(r.Body).Decode(&department); err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	departPublicID, err := h.Store.CreateDepartment(ctx, &department)
+	if err != nil {
+		http.Error(w, "Failed to create department", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"department_id": departPublicID, "department_name": department.Name})
+}
+
+func (h *AuthHandler) GetAllOrgDepartments(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	claims, ok := ctx.Value("claims").(*models.Claims)
+	if !ok {
+		http.Error(w, "Failed to get claims from the token", http.StatusInternalServerError)
+		return
+	}
+	userID := claims.Subject
+
+	// Check if the user is an admin.
+	if !h.Store.CheckUserIsOrgAdmin(ctx, userID) {
+		http.Error(w, "User doesn't have admin privileges", http.StatusUnauthorized)
+		return
+	}
+
+	orgID := claims.OrgID
+
+	departments, err := h.Store.GetAllOrgDepartments(ctx, orgID)
+	if err != nil {
+		http.Error(w, "Failed to get organisation departments", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(departments)
+}
+
+func (h *AuthHandler) UpdateUserDepartment(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	claims, ok := ctx.Value("claims").(*models.Claims)
+	if !ok {
+		http.Error(w, "Failed to get claims from the token", http.StatusInternalServerError)
+		return
+	}
+	userID := claims.Subject
+
+	// Check if the user is an admin.
+	if !h.Store.CheckUserIsOrgAdmin(ctx, userID) {
+		http.Error(w, "User doesn't have admin privileges", http.StatusUnauthorized)
+		return
+	}
+
+	var department models.UpdateUserDepartment
+
+	if err := json.NewDecoder(r.Body).Decode(&department); err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	err := h.Store.UpdateUserDepartment(ctx, department.UserID, department.ID)
+	if err != nil {
+		http.Error(w, "Failed to update user department", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *AuthHandler) DeleteDepartment(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	claims, ok := ctx.Value("claims").(*models.Claims)
+	if !ok {
+		http.Error(w, "Failed to get claims from the token", http.StatusInternalServerError)
+		return
+	}
+	userID := claims.Subject
+
+	// Check if the user is an admin.
+	if !h.Store.CheckUserIsOrgAdmin(ctx, userID) {
+		http.Error(w, "User doesn't have admin privileges", http.StatusUnauthorized)
+		return
+	}
+
+	var department models.DeleteDepartment
+
+	if err := json.NewDecoder(r.Body).Decode(&department); err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	err := h.Store.DeleteDepartment(ctx, department.ID)
+	if err != nil {
+		http.Error(w, "Failed to delete department", http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
